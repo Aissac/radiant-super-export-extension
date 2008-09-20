d6271d33cb0a2b5934a90e993f5c320068a5a787
Istvan Hoka <istvan.hoka@gmail.com>
2008-09-20 22:30:23 +0300

    added description

--- super_export_extension.rb
+++ super_export_extension.rb
@@ -1,21 +1,12 @@
-# Uncomment this if you reference any of your controllers in activate
-# require_dependency 'application'
-
 class SuperExportExtension < Radiant::Extension
-  version "1.0"
-  description "Describe your extension here"
-  url "http://yourwebsite.com/super_export"
-  
-  # define_routes do |map|
-  #   map.connect 'admin/super_export/:action', :controller => 'admin/super_export'
-  # end
+  version "0.1"
+  description "Import/export for Radiant databases with source control friendlyness."
+  url "http://github.com/ihoka/radiant-super-export-extension"
   
   def activate
-    # admin.tabs.add "Super Export", "/admin/super_export", :after => "Layouts", :visibility => [:all]
   end
   
   def deactivate
-    # admin.tabs.remove "Super Export"
   end
   
 end
