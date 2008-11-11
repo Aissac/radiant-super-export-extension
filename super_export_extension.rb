14d4694834c99134f1a93da1fce3fdf5d91bdc7f
Nikos Dimitrakopoulos <nospam@nikosd.com>
2008-11-12 00:11:41 +0200

    Added usage example for production mode plus minor url fix in extension description.

--- super_export_extension.rb
+++ super_export_extension.rb
@@ -1,7 +1,7 @@
 class SuperExportExtension < Radiant::Extension
   version "0.1"
   description "Import/export for Radiant databases with source control friendlyness."
-  url "http://github.com/ihoka/radiant-super-export-extension"
+  url "http://github.com/Aissac/radiant-super-export-extension"
   
   def activate
   end
