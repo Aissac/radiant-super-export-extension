520975f5c13814b25b77b87eb6eeebeee470a5e2
Chris Parrish <chris.parrish-gitmail@swankinnovations.com>
2008-11-23 04:45:39 +0800

    Detect models that have a plural name (per allowed Rails naming rules)
    Signed-off-by: Istvan Hoka <istvan.hoka@gmail.com>

--- lib/super_export.rb
+++ lib/super_export.rb
@@ -1,10 +1,14 @@
 module SuperExport
   EXPORT_ROOT = "#{RAILS_ROOT}/db/export"
   
+  # In most cases singularizing a table's name yields the model's name but in
+  # special cases where the model's name is already seen as plural by rails, it
+  # expects the table's name to then also be plural (so the model: 'MyData' goes
+  # with the table: 'my_data')
   def self.models
     tables = ActiveRecord::Base.connection.tables
     tables.delete "config"
-    models = tables.collect { |table| table.camelize.singularize.constantize rescue nil}.compact
+    models = tables.collect { |table| table.camelize.singularize.constantize rescue nil || table.camelize.constantize rescue nil}.compact
     models << Radiant::Config
     models << Radiant::ExtensionMeta
   end
