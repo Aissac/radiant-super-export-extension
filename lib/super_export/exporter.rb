2ee7fc7d63f9f7331e765a8062ca79a1e1958abd
Istvan Hoka <istvan.hoka@gmail.com>
2009-08-11 12:23:11 +0300

    Fix to properly export multiline strings.

--- lib/super_export/exporter.rb
+++ lib/super_export/exporter.rb
@@ -1,3 +1,20 @@
+# Patch for YAML correct export of multiline strings where:
+#  * The string begins with a newline character
+#  * The first non-empty line begins with an indent
+#  * At least one successive line begins without this same indent
+#
+# Read more: http://blog.smartlogicsolutions.com/2008/09/04/ruby-patch-to-fix-broken-yamldump-for-multi-line-strings-stringto_yaml/
+class String
+  alias :old_to_yaml :to_yaml
+  def to_yaml(options={})
+    if self =~ /^[\n\r]/
+      " #{self}".old_to_yaml(options)
+    else
+      old_to_yaml(options)
+    end
+  end
+end
+
 module SuperExport
   class Exporter
     include SharedMethods
